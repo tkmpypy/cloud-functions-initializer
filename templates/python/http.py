5ee8ef29c66a72452918b7f68c620a5d84815fac
tkmpypy <tkmpypy@gmail.com>
2023-12-15 16:07:08 +0900

    docs(python): Update doc comment

--- templates/python/http.py
+++ templates/python/http.py
@@ -3,7 +3,20 @@ import functions_framework
 # Register an HTTP function with the Functions Framework
 @functions_framework.http
 def my_http_function(request):
-  # Your code here
+    """HTTP Cloud Function.
+    Args:
+        request (flask.Request): The request object.
+        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
+    Returns:
+        The response text, or any set of values that can be turned into a
+        Response object using `make_response`
+        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
+    Note:
+        For more information on how Flask integrates with Cloud
+        Functions, see the `Writing HTTP functions` page.
+        <https://cloud.google.com/functions/docs/writing/http#http_frameworks>
+    """
+    # Your code here
 
-  # Return an HTTP response
-  return 'OK'
+    # Return an HTTP response
+    return 'OK'
